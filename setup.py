3ec97db1407fb62f12a3bbfae36a036904873bac
ksamuel <info.ksamuel@gmail.com>
2011-03-02 18:54:49 +0530

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+setup(name='datadict',
+      version='0.1',
+      description='Remote data dictionary providing shareable definition',
+      author='Mangroveorg',
+      author_email='mangrove@googlegroups.com',
+      url='https://github.com/mangroveorg/datadict',
+      packages=find_packages('.'),
+      install_requires=['CouchDB'],
+      licence='BSD'
+)
+
