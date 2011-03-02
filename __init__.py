ceebbdaba7469d053bc1953713e2bfc6d99b5c12
ksamuel <info.ksamuel@gmail.com>
2011-03-02 18:46:52 +0530

    Saving

--- __init__.py
+++ __init__.py
@@ -2,3 +2,14 @@
 # -*- coding: utf-8 -*-
 # vim: ai ts=4 sts=4 et sw=4
 
+"""
+    Setup the connection singleton
+"""
+
+import connection
+
+if not hasattr(connection, 'db'):
+    url, server, db = connection.connect()
+    connection.url = url
+    connection.server = server
+    connection.db = db
